b9ad4821c536aaebba0acbebd874b453af773f4f
AJ <aj.shirvani@gmail.com>
2013-05-17 22:51:01 -0400

    some modification

--- analysis/script/bn_simple_model.r
+++ analysis/script/bn_simple_model.r
@@ -10,15 +10,26 @@ prepare_data_for_bnlearn <- function(dat) {
 	# convert data to (1,2,3,4) format
 	dat = apply(dat, c(1,2), function(x) { x - 96 })
 
-	payoff_schema = matrix(c(-0.86, -0.86, -1.47,
-							 -0.86, 0.86, 0.34,
-							 0.86, -0.86, 0.79,
-							 0.86, 0.86, 0.34 ), nrow = 4, byrow = T);
+# 	payoff_schema = matrix(c(-0.86, -0.86, -1.47,
+# 							 -0.86, 0.86, 0.34,
+# 							 0.86, -0.86, 0.79,
+# 							 0.86, 0.86, 0.34 ), nrow = 4, byrow = T);
+
+	payoff_schema = matrix(c(-250, 0.5, 2.013,
+							 -250, 0.9, 15.625,
+							 250, 0.5, 0.277,
+							 250, 0.9, 0.625), nrow = 4, byrow = T);
+	rownames(payoff_schema) = c("A", "B", "C", "D");
+	colnames(payoff_schema) = c("outcome", "gain", "loss");
+	# normalize payoff_schema
+	payoff_schema = apply(payoff_schema, 2, function(x) { (x - mean(x)) / sd(x)});
+	print(payoff_schema);
 
 	# convert user selections to feature weight in each trial
 #	dat = head(dat, 2)
 	pat = apply(dat, 1, function(x) { lapply(x, function(y) {
-											 ret = matrix(payoff_schema[y, ], nrow = 1);
+											 index_to_deck = list("A", "B", "C", "D");
+											 ret = matrix(payoff_schema[index_to_deck[[y]], ], nrow = 1);
 											 colnames(ret) = c("outcome", "gain", "loss");
 											 ret
 							 })});
@@ -31,14 +42,23 @@ prepare_data_for_bnlearn <- function(dat) {
 	######## discretize values ###########
 	disc_labels = c("very low", "low", "norm", "high", "very high");
 	disc_labels = seq(5);
-	outcome_seq = seq(-69, 69);
-	gain_seq = seq(-59, 59);
-	loss_seq = seq(-59, 59);
-	outcome_disc = cut(outcome_seq, 5, disc_labels);
-	gain_disc = cut(seq(-59, 59), 5, disc_labels);
-	loss_disc = cut(seq(-59, 59), 5, disc_labels);
+	disc_seq = apply(payoff_schema, 2, function(x) {
+					min_x = sum(x[x<0]) * 40;
+					max_x = sum(x[x>0]) * 40;
+					seq(floor(min_x), ceiling(max_x)); });
+
+	disc = lapply(disc_seq, function(x) { cut(x, 5, disc_labels); });
+	print(disc_seq);
+
+	outcome_seq = disc_seq[["outcome"]];
+	gain_seq = disc_seq[["gain"]];
+	loss_seq = disc_seq[["loss"]];
+	outcome_disc = disc[["outcome"]];
+	gain_disc = disc[["gain"]];
+	loss_disc = disc[["loss"]];
 
 	#	print(tail(pat[[1]]))
+	##### TODO: make it more functional
 	pat = lapply(pat, function(x) { 
 				 ret = apply(x, 1, function(y) {
 							 ret = c(outcome_disc[tail(which(outcome_seq < y["outcome"]), 1)],
@@ -64,7 +84,8 @@ prepare_data_for_bnlearn <- function(dat) {
 
 bn_analysis <- function() {
 	data_file = './bn_data.dat';
-	if (file.exists(data_file)) {
+	reload = F;
+	if (file.exists(data_file) && reload) {
 		print("loading bn data file ...");
 		load(data_file)
 	} else {
@@ -97,7 +118,7 @@ bn_analysis <- function() {
 	#	lapply(bns, function(x) {plot(x); par(ask = T) });
 	png("/tmp/bns_gr.png", width = 800, height = 600);
 	layout(matrix(seq(3), nrow = 1, byrow = T));
-	lapply(bns_gr, function(x) {plot(x); par(ask = T) });
+	lapply(groups, function(x) {plot(bns_gr[[x]], main = x); par(ask = T) });
 	dev.off();
 
 	############ cluster bn analysis  ############
@@ -124,7 +145,6 @@ bn_analysis <- function() {
 				  lapply(gr_names, function(bn) {
 #						 par(ask = T);
 						 main = sprintf("%s, %s, %s", f, gr, bn); 
-						 print(main);
 						 plot(bns_clust[[f]][[gr]][[bn]], main = main);
 									 })
 							 })
